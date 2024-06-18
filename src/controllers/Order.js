14a4b19d862a4f7f398515ddcde24af8dd47c24f
Nguyen Nhat Thong <work.nguyennhatthong@gmail.com>
2024-06-18 15:58:15 +0700

    add router readOrdersByCustomer

--- src/controllers/Order.js
+++ src/controllers/Order.js
@@ -20,21 +20,21 @@ module.exports.POST_Create = async (req, res, next) => {
 
 // /api/order/readMany
 module.exports.GET_ReadMany = async (req, res, next) => {
-    const { sort } = req.query;
+    const { sort = '-finalPrice' } = req.query;
 
     return await Orders.find({})
         .populate([
             { path: 'paymentType', select: 'paymentName' },
             { path: 'customer', select: 'name email phoneNumb' },
             {
-                path: 'products',
-                select: 'productTypeId',
+                path: 'productTypes',
                 populate: {
-                    path: 'productTypeId',
-                    select: 'name',
+                    path: 'products',
+                    select: 'productCode',
                 },
             },
         ])
+        .sort(sort)
         .lean()
         .then((orders) => {
             if (orders.length === 0) {
@@ -67,10 +67,9 @@ module.exports.GET_ReadOne = async (req, res, next) => {
             { path: 'customer', select: 'name email phoneNumb' },
             {
                 path: 'productTypes',
-                // select: 'productTypeId productCode',
                 populate: {
                     path: 'products',
-                    // select: 'name',
+                    select: 'productCode',
                 },
             },
         ])
@@ -101,7 +100,7 @@ module.exports.PUT_UpdateOne = async (req, res, next) => {
     const { _id } = req.params;
     return await Orders.findOneAndUpdate({ _id }, { ...req.body }, { returnOriginal: false })
         .then((order) => {
-            console.log(order)
+            console.log(order);
             if (!order) {
                 return res.status(404).json({
                     success: false,
@@ -162,6 +161,44 @@ module.exports.GET_Search = async (req, res, next) => {
             return res.status(200).json({
                 success: true,
                 msg: `Found ${orders.length} lines`,
+                orders,
+            });
+        })
+        .catch((err) => {
+            return res.status(500).json({
+                success: false,
+                msg: err,
+            });
+        });
+};
+
+// api/order/readOne/:customerId
+module.exports.GET_ReadByCustomer = async (req, res, next) => {
+    const { customer } = req.params;
+
+    return await Orders.find({ customer })
+        .populate([
+            { path: 'paymentType', select: 'paymentName' },
+            { path: 'customer', select: 'name email phoneNumb' },
+            {
+                path: 'productTypes',
+                populate: {
+                    path: 'products',
+                    select: 'productCode',
+                },
+            },
+        ])
+        .lean()
+        .then((orders) => {
+            if (!orders) {
+                return res.status(400).json({
+                    success: false,
+                    msg: `Not found ${_id}`,
+                });
+            }
+            return res.status(200).json({
+                success: true,
+                msg: 'Order is found',
                 orders,
             });
         })
--- src/routers/Order.js
+++ src/routers/Order.js
@@ -6,5 +6,6 @@ router.get('/readMany', orderController.GET_ReadMany);
 router.get('/readOne/:_id', orderController.GET_ReadOne);
 router.put('/updateOne/:_id', orderController.PUT_UpdateOne);
 router.get('/search', orderController.GET_Search);
+router.get('/readMany/:customer', orderController.GET_ReadByCustomer);
 
 module.exports = router;
